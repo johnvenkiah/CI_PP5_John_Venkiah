214787f37d0575a440dd9d4989fadca06ecfd590
johnvenkiah <contact@johnvenkiah.com>
2022-01-16 17:51:15 +0000

    create wishlistadd model in profiles.models

--- profiles/models.py
+++ profiles/models.py
@@ -1,34 +1,57 @@
+
+
+# - - - - - Django Imports - - - - - - - - -
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from django_countries.fields import CountryField
 
+# - - - - - Internal imports - - - - - - - - -
+from products.models import Product
 
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
-    delivery information and order history
+    delivery information and order history.
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE
+    )
     default_phone_number = models.CharField(
         max_length=20,
         null=True,
         blank=True
     )
-    default_street_address1 = models.CharField(max_length=80,
-                                               null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80,
-                                               null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40,
-                                            null=True, blank=True)
-    default_county = models.CharField(max_length=80,
-                                      null=True, blank=True)
-    default_postcode = models.CharField(max_length=20,
-                                        null=True, blank=True)
-    default_country = CountryField(blank_label='Country',
-                                   null=True, blank=True)
+    default_street_address1 = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
+    default_street_address2 = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
+    default_town_or_city = models.CharField(
+        max_length=40,
+        null=True,
+        blank=True
+    )
+    default_county = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
+    default_postcode = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+    default_country = CountryField(
+        blank_label='Country',
+        null=True,
+        blank=True)
 
     def __str__(self):
         return self.user.username
@@ -37,9 +60,23 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or update the user profile
+    Create or update the user profile.
     """
     if created:
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
+
+
+class WishListAdd(models.Model):
+    """
+    A model that keeps track of users wish list items.
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    product = models.ManyToManyField(
+        Product,
+        blank=True
+    )
