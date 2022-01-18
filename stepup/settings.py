c5a52b2c404bddb58af105657eb704d2a17d2bcd
johnvenkiah <contact@johnvenkiah.com>
2022-01-18 17:17:04 +0000

    settings: add and remove comments

--- stepup/settings.py
+++ stepup/settings.py
@@ -14,17 +14,12 @@ import os
 from pathlib import Path
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# This tool was used: https://miniwebtool.com/django-secret-key-generator/
+# Key is in environmental variables
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['stepup-shoes.herokuapp.com', 'localhost']
