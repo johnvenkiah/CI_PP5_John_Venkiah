d6ae192b2f158da5cd941ba15a3f25a917b22fe7
johnvenkiah <contact@johnvenkiah.com>
2022-01-26 18:53:51 +0000

    js: move js from manage_brands to brands_modal.js. Style: backdrop-filter for modal content

---
+++ products/static/products/js/brand_modal.js
@@ -0,0 +1,25 @@
+/**
+ * products/brand_modal.js, file used in manage_brands.html
+ * 
+ * Script to access and display image filename in each modal window.
+ * Elements are given a new class and are iterated to apply the new
+ * filename to.
+*/
+
+/*jshint esversion: 6 */
+/*globals $:false */
+
+$(document).ready(function() {
+    const brandImges = $('.brand-img-class');
+    brandImges.each(function(_, item) {
+        const brandImg = "." + item.classList[0];
+        $(brandImg).change(function(e) {
+            const {target} = e;
+            const {name} = target.files[0];
+            const allFileName = $('.filename-for-brand');
+            allFileName.each(function(_, item) {
+                item.innerHTML =`Set new image to: ${name}`;
+            });
+        });
+    });
+});
